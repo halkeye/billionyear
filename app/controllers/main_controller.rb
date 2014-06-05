80505ee6831e76cac42f56a062ec4a67a33d3e0f
Gavin Mogan <gavin@kodekoan.com>
2014-06-04 22:53:17 -0700

    Main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,13 @@
 class MainController < ApplicationController
   BILLION_SECONDS = 1000000000
+
+  def index
+  end
+  
+  def create
+    redirect_to main_path(params[:id])
+  end
+
   def show
     @time = Time.parse(params[:id]).utc
     @one_billion_time = Time.at(@time.utc.to_i+BILLION_SECONDS)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Billionyear::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'main#index'
+  resources :main
   get ':controller(/:action(/:id))'
 
   # Example of regular route:
