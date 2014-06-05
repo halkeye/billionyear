9f5a17fd4272d2794780ee27702504abcc0c9b37
Gavin Mogan <gavin@kodekoan.com>
2014-06-04 20:23:11 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Billionyear::Application.config.secret_key_base = '1d187027ce787aae7681fc2dfd5e01be68a09a12dfb56f189600b574e728f7749dd5b554b50fc1acc18a0362870e5d39a33da2f91d43d5c3651a8d2709814435'
